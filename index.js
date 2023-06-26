1f6200ca10911a50ac2bfcb22a4e2f463f2e1e91
vimalAdithan <vimaladithanrayz1213s.v.t@gmail.com>
2023-06-26 13:26:12 +0530

    cors

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const app = express();
-app.use(cors({ origin: "*" }));
 // const MONGO_URL = "mongodb://127.0.0.1";
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
@@ -16,6 +15,7 @@ const client = new MongoClient(MONGO_URL); // dial
 // Top level await
 await client.connect(); // call
 console.log("Mongo is connected !!!  ");
+app.use(cors());
 const keysecret = process.env.SECRET_KEY;
 
 app.get("/", async function (request, response) {
